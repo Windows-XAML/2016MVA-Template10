89e9e52e39cffbe7cf5a1e4ae7728b22e168fd96
Daren May <daren.may@gmail.com>
2016-02-18 14:11:36 -0700

    Adding basic news service

---
+++ Source/NewsApp/NewsService/Article.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsService
+{
+    public class Article
+    {
+        public string Headline { get; set; }
+
+        public List<string> Paragraphs { get; set; }
+
+        public string FirstParagraph => Paragraphs[0];
+
+        public List<string> RemainingParagraphs => Paragraphs.GetRange(1, Paragraphs.Count - 1);
+    }
+}
