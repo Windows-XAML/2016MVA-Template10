a4a980121ccdc83382961532938c6a574335c3ee
Jerry Nixon <jerry@nixoncorp.com>
2016-02-18 13:45:20 -0700

    Initial client

---
+++ Source/NewsApp/NewsClient/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using NewsClient.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace NewsClient.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
---
+++ Source/NewsApp/NewsClient/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+using System;
+using NewsClient.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+
+namespace NewsClient.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Enabled;
+        }
+    }
+}
